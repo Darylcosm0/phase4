72cf2e2907a8f1ba082daba208cc5438a5cfe7f2
chris-wamae <chriswamae123@gmail.com>
2023-03-30 19:00:59 +0300

    Fixed error in Sign-up.js

--- src/compoents/form/Sigin-up.js
+++ src/compoents/form/Sigin-up.js
@@ -3,10 +3,9 @@ import Login from "./Login";
 import Register from "./Register";
 
 
-
 function SignUp() {
-
     const [location, setLocation] = useState('register');
+
 
     const toggleForm = (locationUser) => {
         setLocation(locationUser);
@@ -15,20 +14,8 @@ function SignUp() {
 
     return (  
         <section>
-        {
-            location ==='register'? <Register toggleForm={toggleForm}/> : <Login toggleForm={toggleForm}/> 
-
-    const toggleForm = (formUser) => {
-        setLocation(formUser);
-    }
-    return (
-        <section>
-        {
-            location ==='register'? <Register toggleForm={toggleForm}/> : <Login toggleForm={toggleForm}/>
-
-        }
+            {location === 'register' ? <Register toggleForm={toggleForm}/> : location === 'login' ? <Login toggleForm={toggleForm}/> : null}
         </section>
     );
 }
-
 export default SignUp;
