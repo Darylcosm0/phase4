1e235a8cc8d1600b5aa6a1e4c80475a972d94f8b
chris-wamae <chriswamae123@gmail.com>
2023-03-30 18:46:38 +0300

    commit

--- .history/src/compoents/form/Login_20230330115006.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import "./Login.css"; // import your CSS file
-
-function Login(props) {
-  // your component code here
-  
-}
-
-export default Login;
--- .history/src/compoents/navbar/Home_20230329154739.js
+++
@@ -1,6 +0,0 @@
-function displayHome() {
-    console.log('Welcome to the home page!');
-  }
-  
-  module.exports = displayHome;
-  
