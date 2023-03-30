f20ab8eb45f2179f2d28f283fd05086cff30c849
chris-wamae <chriswamae123@gmail.com>
2023-03-30 18:54:50 +0300

    Merged

--- src/compoents/form/Allergy.js
+++ src/compoents/form/Allergy.js
@@ -1,2 +1,17 @@
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+function Allergy(props) {
+const [allergies,setAllergies] = useState([])
+useEffect(() =>{
+    axios.get("").then(r => setAllergies(r.data))
+},[])
+    return (
+       allergies.map((allergy)=>{ 
+        return <li>{allergy.name}</li>
+       })
+    );
+}
 
 export default Allergy;
