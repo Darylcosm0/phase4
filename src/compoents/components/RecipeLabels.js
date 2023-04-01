a4fdff74eca9951d819738ba8155b5017ad76249
chris-wamae <chriswamae123@gmail.com>
2023-04-02 02:43:25 +0300

    Added functionality for removing labels

--- src/compoents/components/RecipeLabels.js
+++ src/compoents/components/RecipeLabels.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { userStore } from '../../data/RecipesStore';
 import {useStore} from "zustand"
+import RemoveLabel from '../subcomponents/RemoveLabel';
 
 function RecipeLabels({recipe}) {
+  const user = useStore(userStore)
       if(recipe.labels !== undefined)
       return (
       recipe.labels.map((label)=>{
-       return <div>
-            <div style={{color:`${label.color}`}}>{label.name}</div>
-            <button>X</button>
-              </div>
+      if(user.user.id == recipe.id){
+        return <div>
+        <div style={{color:`${label.color}`}}>{label.name}</div>
+        <RemoveLabel label_id={label.id} recipe_id={recipe.id}/>
+          </div>
+      }else{
+        return <div>
+        <div style={{color:`${label.color}`}}>{label.name}</div>
+          </div>
+      }
+      
       })
     );
 }
--- src/compoents/subcomponents/RemoveLabel.js
+++ src/compoents/subcomponents/RemoveLabel.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import axios from 'axios';
+import { useStore } from 'zustand';
+import { singleRecipeStore } from '../../data/RecipesStore';
 
-function RemoveLabel(props) {
+function RemoveLabel({recipe_id,label_id}) {
+    console.log(recipe_id)
+    console.log(label_id)
+    const singleStore = useStore(singleRecipeStore)
     return (
-        <div>
-            RemoveLabel
-        </div>
+       <button onClick={()=>{
+        axios.delete(`https://phase-4-project-recipes-backend.onrender.com/recipe_labels`,{
+        recipe_id:recipe_id,
+        label_id:label_id
+        }).then(
+            axios.get(`https://phase-4-project-recipes-backend.onrender.com/recipes/${recipe_id}`).then(
+              r => singleStore.changeSingleRecipe(r.data)
+          ))
+       }}>X</button>
     );
 }
 
