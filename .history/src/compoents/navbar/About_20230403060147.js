d71d08e7a195d34e2af112e951281fbae615f1a4
Darylcosm0 <mutaidaryl@gmail.com>
2023-04-03 06:38:41 +0300

    fixed frontend bugs and dessigned ui

---
+++ .history/src/compoents/navbar/About_20230403060147.js
@@ -0,0 +1,19 @@
+function About() {
+    return (
+      <div className="about">
+        <h2>About our Recipe App</h2>
+        <p>
+          Our Recipe App is a user-friendly web application that makes cooking fun and accessible for everyone. With a vast collection of recipes from all over the world, you can easily search, browse, and save your favorite dishes, whether you're in the mood for a quick and easy meal or a more complex culinary creation.
+        </p>
+        <p>
+          Our team of expert chefs and nutritionists works tirelessly to curate and develop new recipes every week, so you'll never run out of delicious meal ideas. We also provide detailed information about the ingredients, nutritional values, and cooking instructions for each recipe to help you make informed decisions about what to cook and eat.
+        </p>
+        <p>
+          Join our community of food lovers and discover new and exciting dishes to share with your family and friends. If you have any feedback or suggestions, please don't hesitate to reach out to us. We're committed to making your cooking experience as enjoyable and rewarding as possible.
+        </p>
+      </div>
+    );
+  }
+  
+  export default About;
+  xauto
---
+++ .history/src/compoents/navbar/Home_20230403033134.js
@@ -0,0 +1,52 @@
+function Home() {
+  return (
+    <div className="section-a">
+      <div className="container">
+        {/* Image component */}
+        <div className="image-kom">
+          <img src={'https://www.googl'} alt="img" />
+        </div>
+
+        <div className="text">
+          {/* Emojis */}
+          👨‍🍳 🍽️
+          {/* Header component */}
+          <h1 className="mega-font">
+            Hello
+            <span> Foodies</span>
+          </h1>
+          {/* Paragraph component */}
+          <p style={{ fontSize: "30px" }}>
+            An online platform to discover delicious recipes. A collection of recipes from around the world.
+          </p>
+
+          {/* Empty paragraph component */}
+          <p className="number"></p>
+          {/* Horizontal line component */}
+          <hr width="50%" />
+          
+          {/* Column grid component */}
+          <div className="column-grid">
+            <div className="column-1">
+              {/* Column component */}
+              <h3>Appetizers</h3>
+              <p>Starters and finger foods</p>
+            </div>
+            <div className="column-2">
+              {/* Column component */}
+              <h3>Main Dishes</h3>
+              <p>Hearty meals for lunch or dinner</p>
+            </div>
+            <div className="column-3">
+              {/* Column component */}
+              <h3>Desserts</h3>
+              <p>Sweet treats to satisfy your cravings</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
